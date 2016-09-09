3a83871198b66b020933aa7cd86ee2918f1e8706
Chad Metcalf <cm0591@gmail.com>
2016-09-08 23:13:14 -0500

    Add Guard and Pry development dependencies

--- Gemfile
+++ Gemfile
@@ -1,3 +1,6 @@
 source 'https://rubygems.org'
 
+gem 'guard-rspec'
+gem 'pry'
+
 gemspec
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,7 @@
 require 'rspec'
 require 'rspec/its'
 require 'poper'
+require 'pry'
 
 RSpec.configure do |config|
   config.expect_with(:rspec) { |c| c.syntax = :should }
