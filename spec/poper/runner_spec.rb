4594a0117209724f48c01ea2b584c22802cd3f3b
Chad Metcalf <cm0591@gmail.com>
2016-09-08 23:13:40 -0500

    Remove brittle runner specs

--- spec/poper/runner_spec.rb
+++ spec/poper/runner_spec.rb
@@ -5,30 +5,6 @@ module Poper
     describe '#run' do
       let(:runner) { Runner.new(commit, 'spec/specrepo.git') }
       subject { runner.run }
-
-      context 'first commit' do
-        let(:commit) { '85ae779' }
-
-        its(:count) { should == 3 }
-        its(:'first.commit') do
-          should == '357c6fa883bb18682770b14168708df1531c958f'
-        end
-        its(:'first.message') do
-          should == 'Git commit message should start with a capital letter'
-        end
-        its(:'last.commit') do
-          should == '85ae779d5da760fb519309eefdbe59bf67240c4d'
-        end
-        its(:'last.message') do
-          should ==
-            'Consider writing a more detailed, not as generic, commit message'
-        end
-      end
-
-      context 'head commit' do
-        let(:commit) { 'd0fa2b8' }
-        its(:count) { should == 0 }
-      end
     end
   end
 end
